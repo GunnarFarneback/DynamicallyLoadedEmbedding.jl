23595ff77b0a272019a7033cd82ba1b6c31442a4
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2023-06-15 10:25:03 +0200

    Fix declarations of Julia cfunctions.

--- c/julia_cfunctions.h
+++ c/julia_cfunctions.h
@@ -4,12 +4,12 @@
 /* Note, these declarations are function pointers. They can still be
  * called like regular functions without dereferencing.
  */
-double (*julia_sind)(double x);
-int (*mutual_fibonacci)(int n, int (*callback)(int n));
-void (*add_element_number_int)(int *x, int n);
-void (*add_element_number_float)(float *x, int n);
+extern double (*julia_sind)(double x);
+extern int (*mutual_fibonacci)(int n, int (*callback)(int n));
+extern void (*add_element_number_int)(int *x, int n);
+extern void (*add_element_number_float)(float *x, int n);
 
 /* Loads the function pointers above. */
-int load_julia_cfunctions(void);
+extern int load_julia_cfunctions(void);
 
 #endif
